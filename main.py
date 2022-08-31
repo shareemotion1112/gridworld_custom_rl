eea53f0d24efd55b4f0b1c36e01769e9a76f08ed
shareemotion1112 <coolcsg@gmail.com>
2022-08-31 13:41:54 +0900

    add argparse

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 # os.environ['OPENBLAS_NUM_THREADS'] = '8' 
 
+import argparse
+parser = argparse.ArgumentParser(description='gridworld for various algorithms')
+parser.add_argument('--type', '-t', default='dqn')
+args = parser.parse_args()
+
+
 from deep_q_learning import DQN
 
 
-
 # from importlib import reload; reload(env)
-
-dqn = DQN()
-
-dqn.run()
+if args.type == "dqn":
+    dqn = DQN()
+    dqn.run()
 
